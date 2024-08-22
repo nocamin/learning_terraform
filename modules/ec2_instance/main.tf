54aa6bb8e67a72b1e4d2a66b1b942863e6b60473
Radheykant <rbhardwa@gmail.com>
2024-08-22 12:44:34 +0530

    update the main.tf in child module

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -7,13 +7,11 @@ terraform {
   }
 }
 
-provider "aws" {
-  # Empty block or with any common configurations
-}
 resource "aws_instance" "noc" {
   ami           = var.ami_id
   instance_type = var.instance_type
 #  key_name      = var.key_name
 
   tags = var.tags
+  provider = aws
 }
