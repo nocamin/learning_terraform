593a935a80a3967955eeed3447dae9630f4bdcef
Radheykant <rbhardwa@gmail.com>
2024-08-22 13:43:03 +0530

    fix the warning of provider

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,8 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "aws"
+}
+
 resource "aws_instance" "noc" {
   ami           = var.ami_id
   instance_type = var.instance_type
 #  key_name      = var.key_name
 
   tags = var.tags
-  provider = aws
+#  provider = aws
 }
