38459a13878a2687d75b9ed439be523ce2eb2f93
Radheykant <rbhardwa@gmail.com>
2024-08-22 12:41:28 +0530

    add the requird_provider block in child module

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,9 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Ensure this matches your desired AWS provider version
+    }
+  }
+}
+
+provider "aws" {
+  # Empty block or with any common configurations
+}
 resource "aws_instance" "noc" {
   ami           = var.ami_id
   instance_type = var.instance_type
 #  key_name      = var.key_name
 
   tags = var.tags
-
-  provider = aws
 }
