1c4e8db7acb2ab82ec180ef3faa0c033296af014
Radheykant <rbhardwa@gmail.com>
2024-08-24 14:43:48 +0530

    update the variable in shell

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "ec2_us_east_1" {
   ami_id        = var.ami_map["us-east-1"]
   instance_type = var.instance_type
   user_data     = templatefile("${path.module}/templates/user_data.sh", {})
- #s3_bucket_name = var.s3_bucket_name
- #default_region = var.default_region
+  s3_bucket_name = var.s3_bucket_name
+  default_region = var.default_region
   providers = {
     aws = aws.default
   }
@@ -17,8 +17,8 @@ module "ec2_us_west_2" {
   ami_id        = var.ami_map["us-west-2"]
   instance_type = var.instance_type
   user_data     = templatefile("${path.module}/templates/user_data.sh", {})
- #s3_bucket_name = var.s3_bucket_name
- #default_region = var.default_region
+  s3_bucket_name = var.s3_bucket_name
+  default_region = var.default_region
   providers = {
     aws = aws.us-west-2
   }
