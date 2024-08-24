060220c2c59f6a808cfc8ea2a09db91aee00aad5
Radheykant <rbhardwa@gmail.com>
2024-08-24 14:48:42 +0530

    update default variable in module varibale file

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -17,3 +17,13 @@ variable "user_data" {
   description = "User data script"
   type        = string
 }
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "default_region" {
+  description = "The default AWS region"
+  type        = string
+}
