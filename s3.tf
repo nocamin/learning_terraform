2cf3907ae120a1b4876800d9d22ffd3a9005e4dd
Radheykant <rbhardwa@gmail.com>
2024-08-24 13:05:11 +0530

    update the s3 bucket file

--- s3.tf
+++ s3.tf
@@ -1,22 +1,24 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.0.0"
-  region = var.default_region
-  bucket_name = var.s3_bucket_name
-  providers = { aws = aws.default }
-}
+# Create S3 bucket to store Ansible playbooks
+module "noc_services_s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.1"
 
-resource "aws_s3_bucket" "ansible_bucket" {
   bucket = var.s3_bucket_name
-
-  tags = {
-    Name = "Ansible Roles Bucket"
-  }
 }
 
-resource "aws_s3_bucket_object" "ansible_zip" {
-  bucket = aws_s3_bucket.ansible_bucket.bucket
-  key    = "ansible/playbook.zip"
-  source = "${path.module}/ansible/playbook.zip"
+# Zip the Ansible playbook directory
+data "archive_file" "ansible_playbook" {
+  type        = "zip"
+  source_dir  = "${path.module}/ansible/playbook"
+  output_path = "${path.module}/ansible/playbook.zip"
+}
+
+# Upload the zipped playbook to the S3 bucket
+resource "aws_s3_object" "ansible_playbook" {
+  bucket = module.noc_services_s3_bucket.id
+  key    = "playbook.zip"
+  source = data.archive_file.ansible_playbook.output_path
+
+  etag = data.archive_file.ansible_playbook.output_md5
 }
 
