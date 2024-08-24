6e5fc99290eca6a3294146dada35c73e09d1011a
Radheykant <rbhardwa@gmail.com>
2024-08-24 12:23:57 +0530

    update the main file for provider mapping

--- main.tf
+++ main.tf
@@ -1,30 +1,21 @@
+locals {
+  # Create a map that links each region to the actual provider object
+  provider_map = {
+    for region, alias in var.region_provider_map :
+    region => aws[alias]
+  }
+}
+
 module "ec2_instances" {
-  source = "./modules/ec2"
+  source        = "./modules/ec2"
 
-  for_each       = var.region_ami_map
-  region         = each.key
-  ami_id         = each.value
-  instance_type  = var.instance_type
-  user_data      = templatefile("${path.module}/templates/user_data.sh", {})
+  for_each      = var.region_ami_map
+  region        = each.key
+  ami_id        = each.value
+  instance_type = var.instance_type
+  user_data     = templatefile("${path.module}/templates/user_data.sh", {})
+
   providers = {
-    aws = aws[lookup(var.region_provider_map, each.key)]
-  }  
-
-#  providers = {
-#    aws = lookup(
-#      map(
-#        "us-east-1", aws.us-east-1,
-#        "us-west-2", aws.us-west-2,
-#        "eu-west-1", aws.eu-west-1,
-#        "ap-southeast-1", aws.ap-southeast-1,
-#        "ap-southeast-2", aws.ap-southeast-2,
-#        "ap-northeast-1", aws.ap-northeast-1,
-#        "sa-east-1", aws.sa-east-1
-#        # Add other regions and aliases...
-#      ), each.key)
-#  }
-#
-  #providers = {
-  #  aws = aws[lookup(var.providers_list, index(keys(var.region_ami_map), each.key))]
-  # }
+    aws = local.provider_map[each.key]
+  }
 }
