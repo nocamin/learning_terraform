296e0793f1aa32632abd6a3575629d921a14a13e
Radheykant <rbhardwa@gmail.com>
2024-08-24 14:56:50 +0530

    update the main.tf with variable userdata

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ module "ec2_us_east_1" {
   region        = "us-east-1"
   ami_id        = var.ami_map["us-east-1"]
   instance_type = var.instance_type
-  user_data     = templatefile("${path.module}/templates/user_data.sh", {})
-  s3_bucket_name = var.s3_bucket_name
-  default_region = var.default_region
+  user_data = templatefile("${path.module}/templates/user_data.sh", {
+    s3_bucket_name = var.s3_bucket_name,
+    default_region = var.default_region
+  })
   providers = {
     aws = aws.default
   }
@@ -16,9 +17,10 @@ module "ec2_us_west_2" {
   region        = "us-west-2"
   ami_id        = var.ami_map["us-west-2"]
   instance_type = var.instance_type
-  user_data     = templatefile("${path.module}/templates/user_data.sh", {})
-  s3_bucket_name = var.s3_bucket_name
-  default_region = var.default_region
+  user_data = templatefile("${path.module}/templates/user_data.sh", {
+    s3_bucket_name = var.s3_bucket_name,
+    default_region = var.default_region
+  })
   providers = {
     aws = aws.us-west-2
   }
