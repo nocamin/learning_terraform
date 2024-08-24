2e9fb489901beb73a01f57311fd39ac3923faf2a
Radheykant <rbhardwa@gmail.com>
2024-08-24 15:18:11 +0530

    comment out more provider for testing

--- providers.tf
+++ providers.tf
@@ -7,28 +7,28 @@ provider "aws" {
   alias  = "us-west-2"
   region = "us-west-2"
 }
-
-provider "aws" {
-  alias  = "eu-west-1"
-  region = "eu-west-1"
-}
-
-provider "aws" {
-  alias  = "ap-southeast-1"
-  region = "ap-southeast-1"
-}
-
-provider "aws" {
-  alias  = "ap-southeast-2"
-  region = "ap-southeast-2"
-}
-
-provider "aws" {
-  alias  = "ap-northeast-1"
-  region = "ap-northeast-1"
-}
-
-provider "aws" {
-  alias  = "sa-east-1"
-  region = "sa-east-1"
-}
+#
+#provider "aws" {
+#  alias  = "eu-west-1"
+#  region = "eu-west-1"
+#}
+#
+#provider "aws" {
+#  alias  = "ap-southeast-1"
+#  region = "ap-southeast-1"
+#}
+#
+#provider "aws" {
+#  alias  = "ap-southeast-2"
+#  region = "ap-southeast-2"
+#}
+#
+#provider "aws" {
+#  alias  = "ap-northeast-1"
+#  region = "ap-northeast-1"
+#}
+#
+#provider "aws" {
+#  alias  = "sa-east-1"
+#  region = "sa-east-1"
+#}
