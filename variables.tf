1620778ae2015031e25314a426e9b91028be320e
Radheykant <rbhardwa@gmail.com>
2024-08-24 15:24:47 +0530

    fix the extra provider

--- variables.tf
+++ variables.tf
@@ -10,27 +10,27 @@ variable "ami_map" {
   default = {
     "us-east-1"      = "ami-0e86e20dae9224db8"
     "us-west-2"      = "ami-05134c8ef96964280"
-    "eu-west-1"      = "ami-03cc8375791cb8bcf"
-    "ap-southeast-1" = "ami-01811d4912b4ccb26"
-    "ap-southeast-2" = "ami-0892a9c01908fafd1"
-    "ap-northeast-1" = "ami-0cab37bd176bb80d3"
-    "sa-east-1"      = "ami-01a38093d387a7497"
+#    "eu-west-1"      = "ami-03cc8375791cb8bcf"
+#    "ap-southeast-1" = "ami-01811d4912b4ccb26"
+#    "ap-southeast-2" = "ami-0892a9c01908fafd1"
+#    "ap-northeast-1" = "ami-0cab37bd176bb80d3"
+#    "sa-east-1"      = "ami-01a38093d387a7497"
     
   }
 }
 
-variable "region_provider_map" {
-  type = map(string)
-  default = {
-    "us-east-1"      = "default"
-    "us-west-2"      = "us-west-2"
-    "eu-west-1"      = "eu-west-1"
-    "ap-southeast-1" = "ap-southeast-1"
-    "ap-southeast-2" = "ap-southeast-2"
-    "ap-northeast-1" = "ap-northeast-1"
-    "sa-east-1"      = "sa-east-1"
-  }
-}
+#variable "region_provider_map" {
+#  type = map(string)
+#  default = {
+#    "us-east-1"      = "default"
+#    "us-west-2"      = "us-west-2"
+#    "eu-west-1"      = "eu-west-1"
+#    "ap-southeast-1" = "ap-southeast-1"
+#    "ap-southeast-2" = "ap-southeast-2"
+#    "ap-northeast-1" = "ap-northeast-1"
+#    "sa-east-1"      = "sa-east-1"
+#  }
+#}
 
 #variable "providers_list" {
 #  type = list(string)
