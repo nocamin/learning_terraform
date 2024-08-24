366c7df3add467524cf6216ddaecd83dcb5b4ec2
Radheykant <rbhardwa@gmail.com>
2024-08-24 12:10:00 +0530

    Again update the provider in main file

--- main.tf
+++ main.tf
@@ -7,6 +7,20 @@ module "ec2_instance" {
   instance_type  = var.instance_type
   user_data      = templatefile("${path.module}/templates/user_data.sh", {})
   providers = {
-    aws = aws[lookup(var.providers_list, index(keys(var.region_ami_map), each.key))]
+    aws = lookup(
+      map(
+        "us-east-1", aws.us-east-1,
+        "us-west-2", aws.us-west-2,
+        "eu-west-1", aws.eu-west-1,
+        "ap-southeast-1", aws.ap-southeast-1,
+        "ap-southeast-2", aws.ap-southeast-2,
+        "ap-northeast-1", aws.ap-northeast-1,
+        "sa-east-1", aws.sa-east-1
+        # Add other regions and aliases...
+      ), each.key)
   }
+
+  #providers = {
+  #  aws = aws[lookup(var.providers_list, index(keys(var.region_ami_map), each.key))]
+  # }
 }
