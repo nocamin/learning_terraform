bd3f44cc58852d14d8cdcb4100a0dce6cb55dbb1
Radheykant <rbhardwa@gmail.com>
2024-08-25 16:12:00 +0530

    delete output file

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# outputs.tf
-
-output "s3_bucket_name" {
-  value = module.noc_services_s3_bucket.bucket
-}
-
-output "ec2_instance_ids" {
-  value = { for region, mod in module.ec2 : region => mod.instance_id }
-}
