2d4df02ac39f2d76add3d7b9936d9c632730b609
Radheykant <rbhardwa@gmail.com>
2024-08-25 15:36:58 +0530

    edit providers in root structure

--- providers.tf
+++ providers.tf
@@ -1,5 +1,14 @@
 # providers.tf
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
