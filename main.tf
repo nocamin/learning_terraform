f26bdbe416ba2d650a5cccebb9e02af2548d6b8e
Radheykant <rbhardwa@gmail.com>
2024-08-26 18:04:55 +0530

    again fix provider

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ variable "regions" {
 }
 
 provider "aws" {
-  alias  = "regional"
+  alias  = "var.regions[count.index]"
   region = var.regions[count.index]
 }
 
 resource "aws_instance" "example" {
   count    = length(var.regions)
-  provider = aws.regional[count.index]
+  provider = aws.regions[count.index]
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   # Other arguments
