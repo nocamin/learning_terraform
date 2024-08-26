d4177e067ad4ce098900a50833463482066efe4c
Radheykant <rbhardwa@gmail.com>
2024-08-26 17:55:36 +0530

    change main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+# providers.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "regions" {
+  type    = list(string)
+  default = ["us-east-1", "us-west-1"]
+}
+
+provider "aws" {
+  alias  = "regional"
+  region = var.regions[count.index]
+}
+
+resource "aws_instance" "example" {
+  count    = length(var.regions)
+  provider = aws.regional[count.index]
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  # Other arguments
+}
+
