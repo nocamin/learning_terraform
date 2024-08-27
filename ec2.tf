29814896e45c221458ec5ca28d79e6e27e3d0a02
Radheykant <rbhardwa@gmail.com>
2024-08-27 15:44:36 +0530

    udpate main file

--- ec2.tf
+++ ec2.tf
@@ -5,11 +5,7 @@ resource "aws_instance" "example" {
   count = length(var.regions)
   ami   = lookup(var.region_ami_map, var.regions[count.index])
   instance_type = "t2.micro"
-
- # providers = {
- #   aws = aws.regions[count.index]
- # }
-
+  provider = aws.regional_
   tags = {
     Name = "example-instance-${var.regions[count.index]}"
   }
