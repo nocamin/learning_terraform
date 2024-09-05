60aa716f42e935508773c0ed4381556da82a37e2
Radheykant <rbhardwa@gmail.com>
2024-09-05 19:14:48 +0530

    remove random id

--- s3.tf
+++ s3.tf
@@ -1,16 +1,16 @@
 #Random number generate for uniqueness of S3 bucket name
 
-resource "random_id" "prefix" {
-  byte_length = 4  # Generates an 8-character string (2 characters per byte)
-}
-
+#resource "random_id" "prefix" {
+#  byte_length = 4  # Generates an 8-character string (2 characters per byte)
+#}
+#
 
 # Create S3 bucket to store Ansible playbooks
 module "noc_services_s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.1"
 
-  bucket = "${random_id.prefix.hex}-${var.default_region}-${var.s3_bucket_name}"  # Combine the random prefix with the bucket name
+  bucket = "${var.default_region}-${var.s3_bucket_name}"  # Combine the random prefix with the bucket name
 }
 
 # Zip the Ansible playbook directory
@@ -22,7 +22,7 @@ data "archive_file" "ansible_playbook" {
 
 # Upload the zipped playbook to the S3 bucket
 resource "aws_s3_object" "ansible_playbook" {
-  bucket = "${random_id.prefix.hex}-${var.default_region}-${var.s3_bucket_name}"      # Reference the bucket from the module
+  bucket = "${var.default_region}-${var.s3_bucket_name}"      # Reference the bucket from the module
   key    = "playbook.zip"
   source = data.archive_file.ansible_playbook.output_path
 
